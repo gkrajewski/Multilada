ff0d012cd5986da1b31caf245ee7da3784fc7518
gkrajewski <krajewski@psych.uw.edu.pl>
2024-02-04 22:26:39 +0100

    fm_grading() added

--- R/fm_process.R
+++ R/fm_process.R
@@ -40,3 +40,27 @@ fm_matrix <- function(data, names_prefix = NULL, keep = FALSE) {
           dplyr::arrange(.data$rowid) %>%
           dplyr::select(- .data$rowid)
 }
+
+#' @rdname fm_matrix
+#' @export
+fm_grading <- function(data, names_prefix = NULL, keep = FALSE) {
+     if(is.null(names_prefix)) colnames(data)[2] -> names_prefix
+     pattern <- stringr::regex("(?<varname>[^:, ]+?)
+                               :\\s
+                               (?<value>[^:, ]+?)
+                               ,\\s", comments = TRUE)
+
+     stringr::str_match_all(data[, 2], pattern) %>% purrr::map(function(x) {
+          as.data.frame(x) %>%
+               dplyr::select(- .data$V1)
+     }) %>% purrr::list_rbind(names_to = "rowid") %>%
+          tidyr::pivot_wider(names_from = .data$varname, values_from = .data$value,
+                             names_prefix = paste0(names_prefix, "_"),
+                             names_repair = "universal_quiet") -> result
+
+     if(! keep) data %>% dplyr::select(1) -> data
+     data %>% tibble::rowid_to_column() %>%
+          dplyr::left_join(result, by = "rowid") %>%
+          dplyr::arrange(.data$rowid) %>%
+          dplyr::select(- .data$rowid)
+}
