8f3307e43473fbd212b64e6678f503d53c885829
gkrajewski <krajewski@psych.uw.edu.pl>
2025-02-20 21:24:19 +0100

    Form data processing independent from data source

--- R/fm_db.R
+++ R/fm_db.R
@@ -20,16 +20,24 @@ RMariaDB::dbDisconnect(connection)
 
 #### Processing FormMaker data dumped from MySQL ####
 
-form_name <- "IRMIK3"
 form_file_name <- "wor2969_formmaker.csv"
 responses_file_name <- "wor2969_formmaker_submits_20250215.csv"
 
-readr::read_csv(form_file_name, col_select=c(id, title, form_fields)) |>
-     dplyr::filter(title == form_name) -> form
+readr::read_csv(form_file_name) -> formmaker_forms
+readr::read_csv(responses_file_name) -> formmaker_submits
 
+
+#### Extracting given form and its data ####
+
+form_name <- "PABIQ-SCR-PL - Copy"
+
+formmaker_forms |>
+     dplyr::select(id, title, form_fields) |>
+     dplyr::filter(title == form_name) -> form
 form_fields <- tibble::as_tibble(stringr::str_match_all(form$form_fields, "(?:^|\\*)(?<item>\\d+)\\*:\\*id\\*.+?(?<label>[^*]+)\\*:\\*w_field_label")[[1]][, 2:3])
 
-readr::read_csv(responses_file_name) |> dplyr::filter(form_id == form$id) |>
+formmaker_submits |>
+     dplyr::filter(form_id == form$id) |>
      dplyr::mutate(element_label = as.character(element_label)) |>
      dplyr::left_join(form_fields, dplyr::join_by(element_label == item)) -> responses
 
